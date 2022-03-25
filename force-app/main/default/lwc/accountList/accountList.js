45a9a45fd2bbef380fc9b6807f3846821c061f02
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-03-25 22:58:40 +0900

    resolved accountList issues

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -3,50 +3,55 @@ import getAccountList from "@salesforce/apex/AppController.getAccountList";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 const columns = [
-  { label: "Id", fieldName: "Id" },
-  { label: "Name", fieldName: "Name" },
-  { label: "AccountNumber", fieldName: "AccountNumber" }
+    { label: "No", fieldName: "No" },
+    { label: "Id", fieldName: "Id" },
+    { label: "Name", fieldName: "Name" },
+    { label: "AccountNumber", fieldName: "AccountNumber" }
 ];
 
 export default class AccountList extends LightningElement {
-  columns = columns;
-  accountList = [];
-  //   @wire(getAccountList)
-  //   getAccountList({ data, error }) {
-  //     if (data) {
-  //       this.accountList = data;
-  //       this.dispatchEvent(
-  //         new ShowToastEvent({
-  //           title: "Yoohoo",
-  //           message: "Here's your data!",
-  //           variant: "success"
-  //         })
-  //       );
-  //       return;
-  //     }
-  //     this.dispatchEvent(
-  //       new ShowToastEvent({
-  //         title: "Error",
-  //         message: error,
-  //         variant: "error"
-  //       })
-  //     );
-  //   }
+    columns = columns;
+    accountList = [];
+    //   @wire(getAccountList)
+    //   getAccountList({ data, error }) {
+    //     if (data) {
+    //       this.accountList = data;
+    //       this.dispatchEvent(
+    //         new ShowToastEvent({
+    //           title: "Yoohoo",
+    //           message: "Here's your data!",
+    //           variant: "success"
+    //         })
+    //       );
+    //       return;
+    //     }
+    //     this.dispatchEvent(
+    //       new ShowToastEvent({
+    //         title: "Error",
+    //         message: error,
+    //         variant: "error"
+    //       })
+    //     );
+    //   }
 
-  connectedCallback() {
-    getAccountList()
-      .then((data) => {
-        this.accountList = data;
-        console.log(data);
-      })
-      .catch((error) => {
-        this.dispatchEvent(
-          new ShowToastEvent({
-            title: "Error",
-            message: error,
-            variant: "error"
-          })
-        );
-      });
-  }
+    connectedCallback() {
+        getAccountList()
+            .then((data) => {
+                let tempList = data.map((item, index) => {
+                    return { ...item, No: index + 1 };
+                });
+                this.accountList = [...tempList];
+                console.log(this.accountList);
+            })
+            .catch((error) => {
+                console.log("error: ", error);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: "Error",
+                        message: error,
+                        variant: "error"
+                    })
+                );
+            });
+    }
 }
