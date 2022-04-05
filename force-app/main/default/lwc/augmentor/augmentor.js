8a5a8e8a1d8a6484dd2b747d017cc7732546216e
Eldor901 <eldor9012545@gmail.com>
2022-04-05 10:28:34 +0500

    types of comunication lwc

---
+++ force-app/main/default/lwc/augmentor/augmentor.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+
+export default class Augmentor extends LightningElement {
+
+  startCounter = 0;
+  handleStartChange(event) {
+    this.startCounter = parseInt(event.target.value);
+  }
+
+  handleMaximizeCounter(event) {
+    const factor = event.target.dataset.factor;
+
+    this.template.querySelector('c-numerator').maximizeCounter(factor);
+  }
+
+}
